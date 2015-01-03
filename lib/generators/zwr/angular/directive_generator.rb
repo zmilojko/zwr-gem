07a67f27f270ac1956fb444c5b219cfadc84f327
Zeljko <zeljko@zwr.fi>
2015-01-03 16:53:05 +0200

    better angular directive generator
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- lib/generators/zwr/angular/directive_generator.rb
+++ lib/generators/zwr/angular/directive_generator.rb
@@ -5,7 +5,7 @@ module Zwr
         ang_file_name = file_name.gsub("_","-")
         dir_class = file_name.gsub("-","_").camelize(:lower)
         create_file "app/assets/javascripts/directives/#{ang_file_name}.js.coffee", <<-FILE.strip_heredoc
-          @#{application_name}.directive '#{dir_class}', ->
+          @#{application_name}.directive '#{dir_class}', ['$timeout', ($timeout) ->
             ret =
               restrict: 'AE'      # also possible class C
               transclude: true    # set to false if ignoring content
@@ -17,7 +17,25 @@ module Zwr
                                   # the scope
                 glyph: '@'        # isolate scope of a variable (in only), passed with 
                                   # an attribute disabled="123"
-              templateUrl: "#{ang_file_name}.html" 
+              link: (scope, elem, attr) ->
+                scope.$watch (scope) ->
+                  scope.ztItem
+                , ->
+                  scope.revertLocal() if scope.ztItem
+                scope.$watch () ->
+                  element[0].focus() if scope.focusMe == 'true'
+                plunker = ->
+                  $timeout ->
+                    scope.focuschange = !scope.focuschange
+                    plunker() 
+                  ,1000
+                plunker()
+              controller: ($scope) ->
+                $scope.status = 0
+                $scope.getItem = ->
+                  $scope.myItem or $scope.$parent.item
+              templateUrl: "#{ang_file_name}.html"
+            ]
           FILE
         create_file "app/assets/javascripts/templates/#{ang_file_name}.html.haml", <<-FILE.strip_heredoc
           .#{ang_file_name}
